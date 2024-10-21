b9eced3d38d8124f234fbcdaf1e8062bcb7c5703
mrochedy <106345479+mrochedy@users.noreply.github.com>
2024-10-21 17:47:21 +0200

    Replace C with c in config.cpp and config.hpp - Add default config values

---
+++ includes/config.hpp
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   config.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mrochedy <mrochedy@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/17 18:00:21 by mrochedy          #+#    #+#             */
+/*   Updated: 2024/10/21 13:43:46 by mrochedy         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include "webserv.hpp"
+
+typedef struct s_location {
+	std::string					path;
+	std::string					root;
+	std::string					index;
+	std::string					cgi_extension;
+	std::string					upload_save;
+	std::vector<std::string>	allowed_methods;
+} t_location;
+
+typedef struct s_server_config {
+	std::string						listen;
+	std::string						server_name;
+	std::string						client_max_body_size;
+	std::map<int, std::string>		error_pages;
+	std::vector<t_location>			locations;
+} t_server_config;
+
+typedef struct s_cluster_config {
+	std::vector<t_server_config>	servers;
+} t_cluster_config;
+
+t_cluster_config	parseConfigFile(std::string path);
+void				printClusterConfig(const t_cluster_config& cluster);
